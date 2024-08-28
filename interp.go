a857a479c68736b66ec727e3719840f1b4dd4783
jie <jieqiyue123@gmail.com>
2024-08-28 15:49:45 +0800

    解析

--- interp.go
+++ interp.go
@@ -4,12 +4,254 @@ import "unsafe"
 
 type InstrFunc func(state *State, instruction *Instruction)
 
+func emptyFunc(state *State, instruction *Instruction) {
+}
+
+func funcAuipc(state *State, instruction *Instruction) {
+	val := int64(state.pc) + int64(instruction.imm)
+	state.gpRegs[instruction.rd] = uint64(val)
+}
+
 func funcLb(state *State, instruction *Instruction) {
+	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
+	addr = int64(ToHost(uint64(addr)))
+
+	p := (*int8)(unsafe.Pointer(uintptr(addr)))
+	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcLh(state *State, instruction *Instruction) {
+	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
+	addr = int64(ToHost(uint64(addr)))
+
+	p := (*int16)(unsafe.Pointer(uintptr(addr)))
+	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcLw(state *State, instruction *Instruction) {
+	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
+	addr = int64(ToHost(uint64(addr)))
+
+	p := (*int32)(unsafe.Pointer(uintptr(addr)))
+	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcLd(state *State, instruction *Instruction) {
+	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
+	addr = int64(ToHost(uint64(addr)))
+
+	p := (*int64)(unsafe.Pointer(uintptr(addr)))
+	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcLbu(state *State, instruction *Instruction) {
 	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
 	addr = int64(ToHost(uint64(addr)))
 
 	p := (*uint8)(unsafe.Pointer(uintptr(addr)))
 	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcLhu(state *State, instruction *Instruction) {
+	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
+	addr = int64(ToHost(uint64(addr)))
+
+	p := (*uint16)(unsafe.Pointer(uintptr(addr)))
+	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcLwu(state *State, instruction *Instruction) {
+	addr := int64(state.gpRegs[instruction.rs1]) + int64(instruction.imm)
+	addr = int64(ToHost(uint64(addr)))
+
+	p := (*uint32)(unsafe.Pointer(uintptr(addr)))
+	state.gpRegs[instruction.rd] = uint64(*p)
+}
+
+func funcAddi(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = uint64(int64(rs1) + imm)
+}
+
+func funcSlli(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = rs1 << (imm & 0x3f)
+}
+
+func funcSlti(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	if int64(rs1) < imm {
+		state.gpRegs[instruction.rd] = 1
+	} else {
+		state.gpRegs[instruction.rd] = 0
+	}
+}
+
+func funcSltiu(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	if uint64(rs1) < uint64(imm) {
+		state.gpRegs[instruction.rd] = 1
+	} else {
+		state.gpRegs[instruction.rd] = 0
+	}
+}
+
+func funcXori(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = uint64(int64(rs1) ^ imm)
+}
+
+func funcSrli(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = rs1 >> (imm & 0x3f)
+}
+
+func funcSrai(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = int64(rs1) >> (imm & 0x3f)
+}
+
+func funcOri(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = rs1 | uint64(imm)
+}
+
+func funcAndi(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = rs1 & uint64(imm)
+}
+
+func funcAddiw(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = uint64(int64(int32(int64(rs1) + imm)))
+}
+
+func funcSlliw(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = uint64(int64(int32(rs1 << (imm & 0x1f))))
+}
+
+func funcSrliw(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = uint64(int64(int32(uint32(rs1 >> (imm & 0x1f)))))
+}
+
+func funcSraiw(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	imm := int64(instruction.imm)
+
+	state.gpRegs[instruction.rd] = uint64(int64(int32(rs1 >> (imm & 0x1f))))
+}
+
+func funcSb(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	p := (*uint8)(unsafe.Pointer(uintptr(ToHost(uint64(int64(rs1) + int64(instruction.imm))))))
+	*p = uint8(rs2)
+}
+
+func funcSh(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	p := (*uint16)(unsafe.Pointer(uintptr(ToHost(uint64(int64(rs1) + int64(instruction.imm))))))
+	*p = uint16(rs2)
+}
+
+func funcSw(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	p := (*uint32)(unsafe.Pointer(uintptr(ToHost(uint64(int64(rs1) + int64(instruction.imm))))))
+	*p = uint32(rs2)
+}
+
+func funcSd(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	p := (*uint64)(unsafe.Pointer(uintptr(ToHost(uint64(int64(rs1) + int64(instruction.imm))))))
+	*p = uint64(rs2)
+}
+func funcAdd(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = rs1 + rs2
+}
+
+func funcSll(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = rs1 << (rs2 & 0x3f)
+}
+
+func funcSlt(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = (int64(rs1) << int64(rs2))
+}
+
+func funcSltu(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = (uint64(rs1) << uint64(rs2))
+}
+
+func funcXor(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = (rs1 ^ rs2)
+}
+
+func funcSrl(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = rs1 >> (rs2 & 0x3f)
+}
+
+func funcOr(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = rs1 | rs2
+}
+
+func funcAnd(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = rs1 & rs2
+}
+
+func funcMul(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = rs1 * rs2
+}
+
+func funcMulh(state *State, instruction *Instruction) {
+	rs1 := state.gpRegs[instruction.rs1]
+	rs2 := state.gpRegs[instruction.rs2]
+	state.gpRegs[instruction.rd] = (uint64(rs1) << uint64(rs2))
 }
 
 type FuncName string
@@ -192,6 +434,31 @@ var InstrFuncs = make(map[FuncName]InstrFunc)
 
 func init() {
 	InstrFuncs[InsnLb] = funcLb
+	InstrFuncs[InsnLh] = funcLh
+	InstrFuncs[InsnLw] = funcLw
+	InstrFuncs[InsnLd] = funcLd
+	InstrFuncs[InsnLbu] = funcLbu
+	InstrFuncs[InsnLhu] = funcLhu
+	InstrFuncs[InsnLwu] = funcLwu
+
+	InstrFuncs[InsnFence] = emptyFunc
+	InstrFuncs[InsnFenceI] = emptyFunc
+
+	InstrFuncs[InsnAddi] = funcAddi
+	InstrFuncs[InsnSlli] = funcSlli
+	InstrFuncs[InsnSlti] = funcSlti
+	InstrFuncs[InsnSltiu] = funcSltiu
+	InstrFuncs[InsnXori] = funcXori
+	InstrFuncs[InsnSrli] = funcSrli
+	InstrFuncs[InsnSrai] = funcSrai
+	InstrFuncs[InsnOri] = funcOri
+	InstrFuncs[InsnAndi] = funcAndi
+	InstrFuncs[InsnAuipc] = funcAuipc
+
+	InstrFuncs[InsnAddiw] = funcAddiw
+	InstrFuncs[InsnSlliw] = funcSlliw
+	InstrFuncs[InsnSrliw] = funcSrliw
+	InstrFuncs[InsnSraiw] = funcSraiw
 }
 
 func ExecBlockInterp(state *State) {
